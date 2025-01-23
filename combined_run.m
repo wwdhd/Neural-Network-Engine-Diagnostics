e15c9f8ed463c407c4e8069a8e9e9fd1ad8e7cb1
Wahyu Widhi Dyatmika <146448699+wwdhd@users.noreply.github.com>
2025-01-24 04:42:01 +0700

    NN finalisation
    Added the entire neural network process, minus the post-processing excel generation

--- combined_run.m
+++ combined_run.m
@@ -2,14 +2,20 @@ clear all
 close all
 clc
 
-% Path to your Excel file
-filename = 'Test HPC.xlsx';
+% Get the full path to the script's folder (current directory)
+currentFolder = fileparts(mfilename('fullpath'));
 
-% Change the xlsx data into matrix
+% Construct the full path to the Excel file
+filename = fullfile(currentFolder, 'Test HPC.xlsx');
+
+% Read the data from the Excel file
 x1 = readmatrix(filename);
 
-% Display the data to validate the readability
-disp('Input Data')
+% Add the function folder path
+addpath(fullfile(currentFolder, 'Neural-Network-Function'));
+
+% Display the data to validate readability
+disp('Input Data:')
 disp(x1);
 
 
@@ -22,7 +28,9 @@ disp('Detection Output:');
 round(y_det)
 
 % Combined the result of the detection with the result of the detection
-combinedmat = [x1, round(y_det)];
+numbered_column = (1:size(y_det, 1))'; %Creating the case number for the ease of analysis
+combinedmat = [numbered_column, x1, round(y_det)];
+
 
 % Create a filtered matrix, for only the failed detection cases
 % Initialize an empty array to store rows where the first element is < 5
@@ -30,17 +38,19 @@ combinedfiltered = [];
 
 %Filter the rows with loops
 for i = 1:size(combinedmat, 1)
-    if combinedmat(i, 14) > 0
+    if combinedmat(i, 15) > 0
         combinedfiltered = [combinedfiltered; combinedmat(i, :)];  % Append the entire row to B if the first element is = 1
     end
 end
+
 
 %disp(combinedfiltered);
 
 %% ISOLATION
 
 % Take the result of the detection out
-x2 = combinedfiltered(:, 1:13); %x2 only filled with failed cases
+x2 = combinedfiltered(:, 2:14); %x2 only filled with failed cases
+numbered_column2 = combinedfiltered(:, 1);
 
 % Call the neural network function to execute isolation
 y_iso = round(Isolation1440x4v1(x2));
@@ -54,8 +64,15 @@ y_iso(abs(y_iso) == 2 | abs(y_iso) == 3) = 0;
 % Display the modified matrix
 disp(abs(y_iso));
 
+% Display the matrix as an image
+% figure;
+% imagesc(abs(y_iso)); % Display the matrix as a scaled image
+% colormap(gray); % Set the colormap to grayscale
+% axis equal tight; % Adjust the axes
+% title('Grid Representation of y\_iso');
+
 %Combine the x2 with y_iso
-combinedmat2 = [x2, abs(y_iso)];
+combinedmat2 = [numbered_column2, x2, abs(y_iso)];
 
 % Divide the combined matrix into 4 subsets
 iso_lpc = combinedmat2(y_iso(:, 1) == 1, :); % Rows where the LPC column of y_iso is 1
@@ -66,10 +83,71 @@ iso_lpt = combinedmat2(y_iso(:, 4) == 1, :); % Rows where the LPT column of y_is
 %% Quantification, LPC
 
 % Take the result of the isolation out
-x3_lpc = iso_lpc(:, 1:13);
+x3_lpc = iso_lpc(:, 2:14);
+numbered_column3_lpc = iso_lpc(:,1);
 
-y_qua = Quantification1IPC1440x1v1(x3_lpc);
+y_qua_lpc = Quantification1LPC1440x1v1(x3_lpc);
 
 disp("Quantification Result (LPC)")
-disp(y_qua)
+disp(y_qua_lpc)
+
+combinedmat3_lpc = [numbered_column3_lpc, x3_lpc, y_qua_lpc];
+
+%% Quantification, HPC
+
+% Take the result of the isolation out
+x3_hpc = iso_hpc(:, 2:14);
+numbered_column3_hpc = iso_hpc(:,1);
+
+y_qua_hpc = Quantification2HPC1440x1v1(x3_hpc);
+
+disp("Quantification Result (HPC)")
+disp(y_qua_hpc)
+
+combinedmat3_hpc = [numbered_column3_hpc, x3_hpc, y_qua_hpc];
+
+%% Quantification, HPT
+
+% Take the result of the isolation out
+x3_hpt = iso_hpt(:, 2:14);
+numbered_column3_hpt = iso_lpt(:,1);
+
+y_qua_hpt =Quantification3HPT1440x1v1(x3_hpt);
+
+disp("Quantification Result (HPC)")
+disp(y_qua_hpt)
+
+combinedmat3_hpt = [numbered_column3_hpt, x3_hpt, y_qua_hpt];
+
+%% Quantification, LPT
+
+% Take the result of the isolation out
+x3_lpt = iso_lpt(:, 2:14);
+numbered_column3_lpt = iso_lpt(:,1);
+
+y_qua_lpt = Quantification4LPT1440x1v1(x3_lpt);
+
+disp("Quantification Result (LPT)")
+disp(y_qua_lpt)
+
+combinedmat3_lpt = [numbered_column3_lpt, x3_lpt, y_qua_lpt];
+
+%% Excel Output
+
+% % Headers for each sheet (converted to cell array)
+% headers_A = {'Case-No', 'P Total 3', 'T Total 3', 'P Total 5', 'T Total 5', 'P Total 6', 'T Total 6', 'P Total 10', 'T Total 10', 'P Total 12', 'T Total 12', 'Fuel Flow 0', 'Comp 1 PCN', 'Comp 3 PCN'};
+% 
+% % Assuming A is already defined as a numeric matrix
+% A = [numbered_column, x1];  % Define A based on your data
+% 
+% % Convert numeric data in A to cell array
+% data_A = [headers_A; num2cell(A)];
+% 
+% % Define the filename
+% filename = 'testsheet.xlsx';
+% 
+% % Write data_A to the first sheet with headers
+% writecell(data_A, filename, 'Sheet', 'Input_Case');
+
+
 
